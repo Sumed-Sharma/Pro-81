dd418cc76d660f1479bd2d8fc5fc8a8203896a42
Sumed-Sharma <122160068+Sumed-Sharma@users.noreply.github.com>
2023-11-27 12:54:53 +0530

    Add files via upload

---
+++ TabNavigator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import ionicIcons from 'react-native-vector-icons/ionicIcons'
+import Feed from '../Screens/Feed';
+import CreatePost from '../Screens/CreatePost';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () =>{
+    return(
+        
+    )
+  
+}
